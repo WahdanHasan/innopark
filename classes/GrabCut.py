509cd5953f02643b4715b86f9c319c252e5ae81b
ramaf <ramafadi@hotmail.com>
2021-08-07 18:51:12 +0400

    fix: 🐛 grabCut function working!

--- classes/GrabCut.py
+++ classes/GrabCut.py
@@ -1,14 +1,20 @@
 import numpy as np
 import cv2
 import time
-from classes.camera.Camera import Camera
 import classes.system_utilities.image_utilities.ImageUtilities as IU
+from classes.camera.Camera import Camera
 
 #img = cv2.imread("data\\reference footage\\images\\Parked_Car.png")
 
 def getBlackMask(img, bbox):
+	# get cropped img
+	cropped_img, enlarged_bbox = getCroppedImageAndEnlargedBbox(img, bbox)
+
+	#get the resized bbox
+	resized_bbox = getResizedBbox(bbox, enlarged_bbox)
+
 	# create an empty mask according to the shape of img
-	mask = np.zeros(img.shape[:2], np.uint8)
+	mask = np.zeros(cropped_img.shape[:2], np.uint8)
 
 	# create empty arrays where the bg and fg will be stored
 	bgModel = np.zeros((1, 65), np.float64)
@@ -17,19 +23,50 @@ def getBlackMask(img, bbox):
 
 	# apply GrabCut using the the bounding box segmentation method
 	# mask outputs 0 (definite bg), 1 (definite fg), 2 (probable bg), 3 (probable fg)
-	(mask, _, _) = cv2.grabCut(img, mask, bbox, bgModel, fgModel, iterCount=5, mode=cv2.GC_INIT_WITH_RECT)
+	bbox_converted = [resized_bbox[0][0], resized_bbox[0][1], resized_bbox[1][0], resized_bbox[1][1]]
+	#resized_bbox = [20, 40, 19, 79]
+	(mask, _, _) = cv2.grabCut(cropped_img, mask, bbox_converted, bgModel, fgModel, iterCount=7, mode=cv2.GC_INIT_WITH_RECT)
 
 	outputMask = (np.where((mask == cv2.GC_BGD) | (mask == cv2.GC_PR_BGD), 1, 0)*255).astype("uint8")
 	return outputMask
 
-#img = cv2.imread("..\\data\\reference footage\\images\\Parked_Car.png")
+def getCroppedImageAndEnlargedBbox(img, bbox):
+	percentage = 0.1
+	height, width, _ = img.shape
+
+	bbox2 = [[bbox[0][0], bbox[0][1]], [bbox[1][0], bbox[1][1]]]
+
+	# increase the TL
+	for y in range(2):
+		bbox2[0][y] = int(bbox2[0][y] - (bbox2[0][y] * percentage))
+		if bbox2[0][y] < 0:
+			bbox2[0][y] = 0
+
+	#increase the BR
+	bbox2[1][0] = int(bbox2[1][0] + (bbox2[1][0] * percentage))
+	if bbox2[1][0] > width:
+		bbox2[1][0] = width
+
+	bbox2[1][1] = int(bbox2[1][1] + (bbox2[1][1] * percentage))
+	if bbox2[1][1] > height:
+		bbox2[1][1] = height
+
+	cropped_img = IU.CropImage(img, bbox2)
+
+	return cropped_img, bbox2
+
+def getResizedBbox(bbox, enlarged_bbox):
+	TL = [bbox[0][0]-enlarged_bbox[0][0], bbox[0][1]-enlarged_bbox[0][1]]
+	BR = [bbox[1][0] - enlarged_bbox[0][0], bbox[1][1]-enlarged_bbox[0][1]]
+
+	bbox2 = [[TL[0], TL[1]], [BR[0], BR[1]]]
+
+	return bbox2
+
 
 def main():
-	cam_parking = Camera(rtsp_link="data\\reference footage\\videos\\Parking_Open_2.mp4", camera_id=0)
+	cam_parking = Camera(rtsp_link="..\\data\\reference footage\\videos\\Parking_Open_2.mp4", camera_id=0)
 
-
-	#rect = (11, 16, 154, 119)
-	#rect = (1, 1, 163, 121)
 	bbox = [[217, 406], [319, 479]]
 
 	start_time = time.time()
@@ -37,7 +74,7 @@ def main():
 	counter = 0
 
 	while True:
-		frame_parking = cam_parking.GetScaledLoopingNextFrame()
+		frame_parking = cam_parking.GetScaledNextFrame()
 
 		#frame_parking_with_bbox = IU.DrawBoundingBox(frame_parking, [bbox])
 
@@ -51,6 +88,7 @@ def main():
 
 		mask = getBlackMask(frame_parking, bbox)
 		cv2.imshow('mask', mask)
+
 		#output = cv2.bitwise_and(img, img, mask=outputMask)
 
 
