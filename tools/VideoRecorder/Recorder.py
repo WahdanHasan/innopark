de89b851c5be689117f5f7f49f23f1ea60ea7335
Wahdan Hasan <50192463+WahdanHasan@users.noreply.github.com>
2021-08-15 00:40:20 +0400

    Added video recording tool
    This tool needs to be modified still and shouldn't be used for the final sample collection

--- tools/VideoRecorder/Recorder.py
+++ tools/VideoRecorder/Recorder.py
@@ -2,60 +2,94 @@ from classes.camera.Camera import Camera
 import cv2
 import time
 
-print("Cameras starting... ")
-cam_1 = Camera(rtsp_link="rtsp://192.168.0.144",
-               camera_id=0)
-
-cam_2 = Camera(rtsp_link="rtsp://192.168.0.139",
-               camera_id=1)
-
-# cam_3 = Camera(rtsp_link="rtsp://192.168.0.197:8080/h264_ulaw.sdp",
-#                camera_id=3)
-
-# cam_1 = cv2.VideoCapture("rtsp://192.168.0.144")
-# cam_2 = cv2.VideoCapture("rtsp://192.168.0.139")
-# cam_3 = cv2.VideoCapture("rtsp://192.168.0.197:8080/h264_ulaw.sdp")
-
-
+cv2.namedWindow("EE")
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-# _, frame_1 = cam_1.read()
-# _, frame_2 = cam_2.read()
-# _, frame_3 = cam_3.read()
+
+# Connect to cameras
+print("Initializing cameras 1")
+cam_1 = Camera(rtsp_link="http://192.168.0.144:4747/video?1920x1080",
+               camera_id=0)
 cam_1_out = cv2.VideoWriter('ip7plus_recording.avi', fourcc, cam_1.feed.get(cv2.CAP_PROP_FPS), (cam_1.GetRawNextFrame().shape[1], cam_1.GetRawNextFrame().shape[0]))
+
+print("Initializing cameras 2")
+cam_2 = Camera(rtsp_link="http://192.168.0.139:4747/video?1920x1080",
+               camera_id=1)
 cam_2_out = cv2.VideoWriter('ip7_recording.avi', fourcc, cam_2.feed.get(cv2.CAP_PROP_FPS), (cam_2.GetRawNextFrame().shape[1], cam_2.GetRawNextFrame().shape[0]))
+
+# print("Initializing cameras 3")
+# cam_3 = Camera(rtsp_link="http://192.168.0.197:4747/video?1920x1080",
+#                camera_id=3)
 # cam_3_out = cv2.VideoWriter('s4_recording.avi', fourcc, cam_3.feed.get(cv2.CAP_PROP_FPS), (cam_3.GetRawNextFrame().shape[1], cam_3.GetRawNextFrame().shape[0]))
-# cam_1_out = cv2.VideoWriter('ip7plus_recording.avi', fourcc, cam_1.get(cv2.CAP_PROP_FPS), (frame_1.shape[1], frame_1.shape[0]))
-# cam_2_out = cv2.VideoWriter('ip7_recording.avi', fourcc, cam_2.get(cv2.CAP_PROP_FPS), (frame_2.shape[1], frame_2.shape[0]))
-# cam_3_out = cv2.VideoWriter('s4_recording.avi', fourcc, cam_3.get(cv2.CAP_PROP_FPS), (frame_3.shape[1], frame_3.shape[0]))
+
+# Initialize array of cameras and video writers
+cameras = [cam_1, cam_2]
+video_writers = [cam_1_out, cam_2_out]
+
 
 print("Started feed...")
 start_time = time.time()
 seconds_before_display = 1  # displays the frame rate every 1 second
 counter = 0
+
+# Initialize list
+frames = []
+cam_count = len(cameras)
+for i in range(cam_count):
+    frames.append(0)
+
 while True:
-    # _, frame_1 = cam_1.read()
-    # _, frame_2 = cam_2.read()
-    # _, frame_3 = cam_3.read()
-    frame_1 = cam_1.GetRawNextFrame()
-    frame_2 = cam_2.GetRawNextFrame()
-    # frame_3 = cam_3.GetRawNextFrame()
+    # Read frames from cameras
+    for i in range(cam_count):
+        frames[i] = cameras[i].GetRawNextFrame()
+        # cv2.imshow(str(i), frames[i])
 
+    # Write frames to video file
+    for i in range(cam_count):
+        video_writers[i].write(frames[i])
 
-    cv2.imshow("x", frame_1)
-
-    cam_1_out.write(frame_1)
-    cam_2_out.write(frame_2)
-    # cam_3_out.write(frame_3)
-
+    # Log fps to console
     counter += 1
     if (time.time() - start_time) > seconds_before_display:
         print("FPS: ", counter / (time.time() - start_time))
         counter = 0
         start_time = time.time()
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        # cam_1.ReleaseFeed()
-        cam_1_out.release()
-        cam_2_out.release()
-        cam_3_out.release()
+        # Release video writers and cameras
+        for i in range(cam_count):
+            video_writers[i].release()
+            cameras[i].ReleaseFeed()
+
         cv2.destroyAllWindows()
         break
+
+# import cv2
+# import numpy as np
+# import classes.system_utilities.image_utilities.ImageUtilities as IU
+# import time
+#
+# url = "http://192.168.0.197:4747/video?1920x1080"
+# # url2= "http://192.168.0.139:4747/video?1920x1080"
+#
+# cap = cv2.VideoCapture(url)
+# # cap2 = cv2.VideoCapture(url2)
+#
+# print("Started feed...")
+# start_time = time.time()
+# seconds_before_display = 1  # displays the frame rate every 1 second
+# counter = 0
+#
+# while True:
+#     _, frame = cap.read()
+#     # _, frame2 = cap2.read()
+#     cv2.imshow("x", frame)
+#     # cv2.imshow("y", frame2)
+#
+#     counter += 1
+#     if (time.time() - start_time) > seconds_before_display:
+#         print("FPS: ", counter / (time.time() - start_time))
+#         counter = 0
+#         start_time = time.time()
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         cv2.destroyAllWindows()
+#         break
