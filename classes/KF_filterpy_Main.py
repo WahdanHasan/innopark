7890407d3ce74393366e12a0bfe6d9e374b71f2e
Rama Sbeinaty <53365736+ramasbeinaty@users.noreply.github.com>
2021-09-13 16:06:24 +0400

    feat: 🎸 delete points with zero velocity
    need to check the reliability of it

--- classes/KF_filterpy_Main.py
+++ classes/KF_filterpy_Main.py
@@ -5,16 +5,18 @@ from classes.camera.Camera import Camera
 import time
 import copy
 
-from KalmanFilter2D import KalmanFilter2D
-
 scale_percentage = 0.5
 
+#cap = cv2.VideoCapture("..\\data\\reference footage\\videos\\Car_Pass_Starting_Full.mp4")
 cap = cv2.VideoCapture("..\\data\\reference footage\\videos\\Leg_2_Starting_Full.mp4")
+#cap = Camera("..\\data\\reference footage\\videos\\Leg_2_Starting_Full.mp4", 0)
+#cap = Camera("..\\data\\reference footage\\videos\\Car_Pass_Starting_Full.mp4", 0)
+#cap = cv2.VideoCapture("..\\data\\reference footage\\videos\\Parking_Open_2.mp4")
 
-#cap = cv2.VideoCapture("..\\data\\reference footage\\videos\\Car_Pass3.mp4")
+#bbox = [[217, 406], [319, 479]]
 
-feature_params = dict(maxCorners=1,
-                      qualityLevel=0.01,
+feature_params = dict(maxCorners=100,
+                      qualityLevel=0.1,
                       minDistance=7,
                       blockSize=3)
 
@@ -22,41 +24,26 @@ lk_params = dict(winSize=(15, 15),
                  maxLevel=3,
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
+
 _, frame = cap.read()
-frame = IU.RescaleImageToScale(frame, scale_factor=scale_percentage)
+frame = cv2.resize(frame, (int(frame.shape[1]*scale_percentage), int(frame.shape[0]*scale_percentage)), interpolation=cv2.INTER_AREA)
 old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# cv2.imshow("frame", frame)
-# cv2.waitKey()
-
+#bbox = [[38, 247], [530,419]] #car_pass_full
 bbox = [[170, 207], [644,395]] #car_leg_full
-bbox_tl = [170, 207]
-bbox_br = [644, 395]
-# bbox = [[99, 85], [187,200]] #car_pass3_before_barrier
-
-# initialize array
-old_points = np.zeros((1, 2))
-
-#mouse function
-def SelectPoint(event, x, y, flags, params):
-    global selectedPoint, point_selected, old_points
-    if event == cv2.EVENT_LBUTTONDOWN:
-        selectedPoint = (int(x), int(y))
-        point_selected = True
-        old_points = np.array([[x, y]], dtype=np.float32)
-
-# cv2.namedWindow("Frame")
-# cv2.setMouseCallback("Frame", SelectPoint)
-# point_selected = False
-# selectedPoint = ()
-
-# cv2.imshow('Frame', frame)
-# cv2.waitKey(0)
+#bbox = [[400, 611], [564,716]] #bbox of Parking_Open_2
+# bbox_tl = [170, 207]
+# bbox_br = [644, 395]
 
 def GetBboxGoodFeatures(bbox, gray):
     cropped_frame = IU.CropImage(gray, bbox)
 
-    points = cv2.goodFeaturesToTrack(cropped_frame, **feature_params)
+    points = cv2.goodFeaturesToTrack(cropped_frame, mask=None,
+                                       maxCorners=5,
+                                       qualityLevel=0.03,
+                                       minDistance=7,
+                                       blockSize=3)
+
 
     # translate old_points of bbox to the non-cropped frame
     for i in range(len(points)):
@@ -68,7 +55,35 @@ def GetBboxGoodFeatures(bbox, gray):
         if points[i][0][1] > frame.shape[0]:
             points[i][0][1] = frame.shape[0]
 
+    # draw the points
+    for i in range(len(points)):
+        a, b = points[i].ravel()
+        img = cv2.circle(frame, (int(a), int(b)), 2, (0, 255, 0), 2)
+        cv2.imshow("initial points", img)
+
     return points
+
+def boundOpticalFlowPoints(frame, points):
+    height, width = frame.shape[:2]
+    leftMost_x = width
+    rightMost_x = 0
+    top_y = height
+    bottom_y = 0
+    for i in range(len(points)):
+        a, b = points[i].ravel()
+        if a < leftMost_x:
+            leftMost_x = a
+        if b < top_y:
+            top_y = b
+        if a > rightMost_x:
+            rightMost_x = a
+        if b > bottom_y:
+            bottom_y = b
+
+    bbox_tl = [int(leftMost_x), int(top_y)]
+    bbox_br = [int(rightMost_x), int(bottom_y)]
+
+    return bbox_tl, bbox_br
 
 def convert_bbox_to_z(bbox):
     """
@@ -84,106 +99,73 @@ def convert_bbox_to_z(bbox):
     r = w/float(h)
     return np.array([x,y,s,r]).reshape((4,1))
 
-def convert_x_to_bbox(x,score=None):
-  """
-  Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
-    [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
-  """
-  w = np.sqrt(x[2]*x[3])
-  h = x[2]/w
-  if(score==None):
-    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
-  # else:
-  #   return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
-
-z = convert_bbox_to_z((bbox[0][0], bbox[0][1], bbox[1][0], bbox[1][1]))
-old_points = np.array([[int(z[0]), int(z[1])]], dtype=np.float32)
-
-#old_points = GetBboxGoodFeatures(bbox, old_gray)
+old_points = GetBboxGoodFeatures(bbox, old_gray)
+bbox_tl, bbox_br = boundOpticalFlowPoints(frame, old_points)
 
 # Create a random color
 color = np.random.randint(0, 255, (100, 3))
 
+# Create a mask img to see optical flow drawing
+mask = np.zeros_like(frame)
+
 start_time = time.time()
 seconds_before_display = 1  # displays the frame rate every 1 second
 counter = 0
-
-frames_num = 1
-x_old = 0; y_old = 0
-ctr = 0
-
-KF = KalmanFilter2D(0.1, 1, 1, 1, 0.1 ,0.1)
+frame_counter = 1
 while True:
-    frames_num+=1
+    frame_counter+=1
+    t1 = time.time() - start_time
     _, frame = cap.read()
-    frame = IU.RescaleImageToScale(frame, scale_factor=scale_percentage)
+    frame = cv2.resize(frame, (int(frame.shape[1] * scale_percentage), int(frame.shape[0] * scale_percentage)), interpolation=cv2.INTER_AREA)
 
     new_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # if point_selected is False:
-    #     continue
-
-    cv2.circle(frame, (int(z[0]), int(z[1])), 5, (0, 0, 255), 2)
-
-    #Predict KF
-    if ctr == 0:
-        x = [355.505, 285.805]
-        y = [355.505, 285.805]
-        ctr+=1
-    else:
-        (x, y) = KF.predict()
-
-    if(frames_num==2): x = x[0] ; y = y[0]
-    else: x = x[0] ; y = y[1]
-
-    bbox_tl[0] = int(bbox_tl[0] + (int(x) - x_old))
-    # bbox_tl[1] = int(bbox_tl[1] + (int(y) - y_old))
-    bbox_br[0] = int(bbox_br[0] + (int(x) - x_old))
-    # bbox_br[1] = int(bbox_br[1] + (int(y) - y_old))
-    #cv2.rectangle(frame, (bbox_tl[0], bbox_tl[1]), (bbox_br[0], bbox_br[1]), (255, 0, 0), 2)
-    frame = IU.DrawBoundingBox(frame, [[bbox_tl, bbox_br]])
-
-    cv2.imshow("Frame bbox", frame)
-    cv2.waitKey(100)
-
-    # cv2.rectangle(frame, (int(x) - 15, int(y) - 15), (int(x) + 15, int(y) + 15), (0, 0, 255), 2)
-    #
-    # cv2.imshow("Frame", frame)
-    # cv2.waitKey(0)
     #Lucas Kenade
     new_points, status, error = cv2.calcOpticalFlowPyrLK(old_gray, new_gray, old_points, None, **lk_params)
+    t2 = time.time() - start_time
 
-    #bbox = [[170, 207], [644, 395]]
+    # draw new bbox
+    change = ((old_points[0][0][0] - new_points[0][0][0]), (old_points[0][0][1] - new_points[0][0][1]))
+    if (int(change[0]) !=0): # if there's no significant change, don't move box
+        bbox_tl, bbox_br = boundOpticalFlowPoints(frame, new_points)
+        # bbox_tl[0] = int(bbox_tl[0] - change[0])
+        # bbox_br[0] = int(bbox_br[0] - change[0])
+        # #bbox_tl[1] = int(bbox_tl[1] - change[1])
+        # #bbox_br[1] = int(bbox_br[1] - change[1])
+        # print("CHANGE HAPPENED")
+    frame1 = IU.DrawBoundingBoxes(frame, [[bbox_tl, bbox_br]])
 
-    # draw the tracks
+    #get rid of inconsistent points
+    print("new: ", new_points)
+    print("old: ", old_points)
+    if(frame_counter % 7 == 0):
+        print("LENGTH: ", len(new_points))
+        for i in range(len(new_points)):
+            a, b = new_points[i].ravel()
+            c, d = old_points[i].ravel()
+            velocity = [((a-c)/(t2-t1)), ((b-d)/(t2-t1))]
+            if (velocity[0] == 0.000 and velocity[1]==0.000):
+                print("element DELETED: ", i)
+                new_points = np.delete(new_points, [i], axis=0)
+                for i in range(len(new_points)):
+                    a, b = new_points[i].ravel()
+                    cv2.circle(frame, (int(a), int(b)), 2, (0, 255, 0), 2)
+                cv2.imshow("Frame_bbox", frame)
+                cv2.waitKey(0)
+                break
+
+
+    # draw the points
     for i in range(len(new_points)):
-        a, b = new_points.ravel()
-        c, d = old_points.ravel()
-        a = int(a);
-        b = int(b);
-        c = int(c);
-        d = int(d)
-        # draws new point
-        frame = cv2.circle(frame, (a, b), 2, (0, 255, 0), 2)
+        a, b = new_points[i].ravel()
+        cv2.circle(frame, (int(a), int(b)), 2, (0, 255, 0), 2)
 
-        #print("a: ", a, "| b: ", b)
-        point = KF.update((a, b))
-        x1 = point[0][0]; y1 = point[1][1]
-        #print("updated points: ", x1, "| ", y1)
-        # Draw a rectangle as the estimated object position
-        cv2.rectangle(frame, (int(x1) - 15, int(y1) - 15), (int(x1) + 15, int(y1) + 15), (0, 0, 255), 2)
-        cv2.putText(frame, "Estimated Position", (int(x1) + 15, int(y1) + 10), 0, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, "Predicted Position", (int(x) + 15, int(y)), 0, 0.5, (255, 0, 0), 2)
-        cv2.putText(frame, "Measured Position", (int(a) + 15, int(b) - 15), 0, 0.5, (0, 191, 255), 2)
-
-    cv2.imshow("Frame1 mask added", frame)
-    #cv2.waitKey(0)
+    cv2.imshow("Frame_bbox", frame)
 
     # update previous frame and old points
+    # so every new frame is being compared to its previous frame
     old_gray = new_gray.copy()
     old_points = new_points.reshape(-1, 1, 2)
-    x_old = int(x)
-    y_old = int(y)
 
     counter += 1
     if (time.time() - start_time) > seconds_before_display:
