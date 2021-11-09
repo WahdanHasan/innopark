8cbc1cdefff42f20227f0280ca57c35f77058d97
ramasbeinaty <ramafadi@hotmail.com>
2021-11-09 05:48:24 +0400

    line is perpendicular in wrong direction

--- classes/system/parking/ParkingViolationManager.py
+++ classes/system/parking/ParkingViolationManager.py
@@ -44,6 +44,9 @@ class ParkingViolationManager(TrackedObjectListener):
              ]
         ]
 
+        self.camera_id_key = "camera_id_"
+        self.parking_status_key = "parking_status"
+
         self.minimum_length_of_vehicle_trail = 0
 
         # self.maximum_tracked_vehicle_bbs = 100
@@ -61,6 +64,7 @@ class ParkingViolationManager(TrackedObjectListener):
         self.frames = []
 
         self.temp_counter = 0
+        self.threshold_percentage = 0.75
 
     def createSharedMemoryStuff(self, amount_of_trackers):
         for i in range(amount_of_trackers):
@@ -109,35 +113,43 @@ class ParkingViolationManager(TrackedObjectListener):
         for i in range(len(bbs)):
             self.temp_counter +=1
             vehicle_plate = ids[2][i]
+            camera_id = ids[1][i]
 
-            # Initialize a list to key vehicle_plate if vehicle doesn't exist
+            # Initialize vehicle_plate dict for new vehicle
             if vehicle_plate not in self.latest_tracked_vehicle_bbs:
-                self.latest_tracked_vehicle_bbs[vehicle_plate] = [ParkingStatus.NOT_OCCUPIED]
+                # initialize the parking status
+                self.latest_tracked_vehicle_bbs[vehicle_plate] = {
+                    self.parking_status_key:ParkingStatus.NOT_OCCUPIED
+                }
+
+                # initialize a list for each camera
+                for camera_details in Constants.CAMERA_DETAILS:
+                    self.latest_tracked_vehicle_bbs[vehicle_plate][self.camera_id_key+str(camera_details[0])] = []
 
             # Build the history of vehicle bbs if vehicle is not parked
-            if self.latest_tracked_vehicle_bbs[vehicle_plate][0] == ParkingStatus.NOT_OCCUPIED:
-                self.buildVehicleBbHistory(bbs=bbs, vehicle_plate=vehicle_plate)
+            if self.latest_tracked_vehicle_bbs[vehicle_plate][self.parking_status_key] == ParkingStatus.NOT_OCCUPIED:
+                self.buildVehicleBbHistory(bbs=bbs, camera_id=camera_id, vehicle_plate=vehicle_plate)
 
             for j in range(len(self.parking_spaces)):
-
                 # if camera_id != self.parking_spaces[j].camera_id:
-                if ids[1][i] != self.parking_spaces[j][0]:
+                if camera_id != self.parking_spaces[j][0]:
                     continue
 
                 # if self.parking_spaces.status == ParkingStatus.OCCUPIED:
                 if self.parking_spaces[j][3] == ParkingStatus.OCCUPIED or self.temp_counter == 200:
                     print("PARKING OCCUPIED AAAAAAAAAAAAAAAAAAAA")
-                    self.latest_tracked_vehicle_bbs[vehicle_plate][0] = ParkingStatus.OCCUPIED
-                    bbs_center_pts = self.calculateCenterOfBbs(vehicle_plate=vehicle_plate)
+                    self.latest_tracked_vehicle_bbs[vehicle_plate][self.parking_status_key] = ParkingStatus.OCCUPIED
+                    # bbs_center_pts = self.calculateCenterOfBbs(vehicle_plate=vehicle_plate, camera_id=camera_id)
+                    bbs_center_pts = self.calculateBottomOfBbs(vehicle_plate=vehicle_plate, camera_id=camera_id)
+                    # bbs_center_pts = self.getBROfBbs(vehicle_plate=vehicle_plate, camera_id=camera_id)
                     self.drawVehicleTrail(bbs_center_pts)
                     break
 
-
     def drawVehicleTrail(self, bbs_center_pts):
-        blank_img = self.base_blank.copy()
+        blank_img = self.calculateMinimumVehicleTrail()
+        # blank_img = self.base_blank.copy()
 
         # format of bbs_center_pt is [center_x, center_y]
-        print("length: ", len(bbs_center_pts))
         for i in range(len(bbs_center_pts)):
             if i % 2 == 0:
                 continue
@@ -147,44 +159,129 @@ class ParkingViolationManager(TrackedObjectListener):
             blank_img = IU.DrawLine(image=blank_img, point_a=(int(center_pt1[0]), int(center_pt1[1])) ,
                         point_b=(int(center_pt2[0]), int(center_pt2[1])), color=(0, 255, 0))
 
+        perpendicular_pts = self.getPerpendicularPointOnLine(bbs_center_pts)
+        print("BERBENDICULAAAAAAAAAAAAAAR: ", perpendicular_pts)
+        blank_img = self.drawVehicleTrailToBottomParking(blank_img, bbs_center_pts, perpendicular_pts)
+
         # cv2.imshow("full trail", blank_img)
-        IU.SaveImage(blank_img,"full trail_4")
+        IU.SaveImage(blank_img,"connect trail to bottom parking_1")
         # cv2.waitKey(0)
 
+    def drawVehicleTrailToBottomParking(self, img, bbs_pts, perpendicular_pts):
+        # blank_img = self.base_blank.copy()
+        blank_img = img.copy()
+
+        threshold_length = math.ceil(len(bbs_pts) * self.threshold_percentage)
+
+        length = len(bbs_pts)
+        perp_length = len(perpendicular_pts)
+        # for i in range(len(perpendicular_pts)):
+        #     blank_img = IU.DrawLine(image=blank_img, point_a=(int(bbs_pts[i+threshold_length][0]), int(bbs_pts[i+threshold_length][1])),
+        #                             point_b=(int(perpendicular_pts[i][0]), int(perpendicular_pts[i][1])), color=(0, 255, 255))
+
+        blank_img = IU.DrawLine(image=blank_img,
+                                point_a=(int(bbs_pts[length-1][0]), int(bbs_pts[length-1][1])),
+                                point_b=(int(perpendicular_pts[perp_length-1][0]), int(perpendicular_pts[perp_length-1][1])),
+                                color=(0, 255, 255))
+
+        blank_img = IU.DrawLine(image=blank_img,
+                                point_a=(int(self.parking_spaces[0][2][2][0]), int(self.parking_spaces[0][2][2][1])),
+                                point_b=(
+                                int(self.parking_spaces[0][2][3][0]), int(self.parking_spaces[0][2][3][1])),
+                                color=(0, 255, 255))
+        return blank_img
+
+
+    def getPerpendicularPointOnLine(self, bbs_pts):
+        # get [x, y] coordinates of a point on the line perpendicular to specified pts
+        perpendicular_pts = []
+        parking_BL = self.parking_spaces[0][2][2]
+        parking_BR = self.parking_spaces[0][2][3]
+        print("BOTTOM LINE PARKING---> (", parking_BL, "," ,parking_BR, ")" )
+
+        threshold_length = math.ceil(len(bbs_pts)*self.threshold_percentage)
+
+        for pt in bbs_pts[threshold_length:]:
+            k = ((pt[0]-parking_BL[0])*(parking_BR[0]-pt[0]) + (pt[1]-parking_BL[1])*(parking_BR[1]-parking_BL[1])) \
+            / ((parking_BR[0]-parking_BL[0])**2 + (parking_BR[1]-parking_BL[1])**2)
+
+            perpendicular_x = parking_BL[0] + k*(parking_BR[0]-parking_BL[0])
+            perpendicular_y = parking_BL[1] + k*(parking_BR[1]-parking_BL[1])
+
+            perpendicular_pts.append([math.ceil(perpendicular_x), math.ceil(perpendicular_y)])
+
+        return perpendicular_pts
+
+
     def calculateMinimumVehicleTrail(self):
+        blank_img = self.base_blank.copy()
+
         for j in range(len(self.parking_spaces)):
             parking_bbs = self.parking_spaces[j][2]
             # math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
             parking_lane_length_left = math.sqrt((parking_bbs[2][0]-parking_bbs[0][0])**2 + (parking_bbs[2][1]-parking_bbs[0][1])**2)
             parking_lane_length_right = math.sqrt((parking_bbs[3][0]-parking_bbs[1][0])**2 + (parking_bbs[3][1]-parking_bbs[1][1])**2)
 
-            if parking_lane_length_left > parking_lane_length_right:
-                self.minimum_length_of_vehicle_trail = parking_lane_length_left
-            else:
-                self.minimum_length_of_vehicle_trail = parking_lane_length_right
+            blank_img = IU.DrawLine(image=blank_img, point_a=(int(parking_bbs[2][0]), int(parking_bbs[2][1])),
+                                    point_b=(int(parking_bbs[0][0]), int(parking_bbs[0][1])), color=(0, 0, 255))
+
+            blank_img = IU.DrawLine(image=blank_img, point_a=(int(parking_bbs[1][0]), int(parking_bbs[1][1])),
+                                    point_b=(int(parking_bbs[3][0]), int(parking_bbs[3][1])), color=(0, 0, 255))
+
+            return blank_img
+
+            # if parking_lane_length_left > parking_lane_length_right:
+            #     self.minimum_length_of_vehicle_trail = parking_lane_length_left
+            # else:
+            #     self.minimum_length_of_vehicle_trail = parking_lane_length_right
 
 
-    def calculateCenterOfBbs(self, vehicle_plate):
+    def calculateBottomOfBbs(self, vehicle_plate, camera_id):
+        bbs_top_midpoints = []
+
+        # calculate the ceiling midpoints
+        for bb in self.latest_tracked_vehicle_bbs[vehicle_plate][self.camera_id_key + str(camera_id)]:
+            # calculate BL
+            BL = [bb[0][0], bb[1][1]]
+
+            # Apply midpoint formula to get midpoint of top bb
+            midpoint = [math.ceil((BL[0]+bb[1][0])/2), math.ceil((BL[1]+bb[1][1])/2)]
+            print("bb: ", bb)
+            print("center: ", midpoint)
+            bbs_top_midpoints.append(midpoint)
+
+        return bbs_top_midpoints
+
+    def getBROfBbs(self, vehicle_plate, camera_id):
+        bbs_BR_pts = []
+
+        for bb in self.latest_tracked_vehicle_bbs[vehicle_plate][self.camera_id_key + str(camera_id)]:
+            bbs_BR_pts.append([bb[1][0], bb[1][1]])
+
+        return bbs_BR_pts
+
+
+    def calculateCenterOfBbs(self, vehicle_plate, camera_id):
         # returns center pts in format [center_x, center_y]
         bbs_center_pts = []
 
-        # remember that bbs start from the second element
         # calculate the ceiling center points
-        for bb in self.latest_tracked_vehicle_bbs[vehicle_plate][1:]:
+        for bb in self.latest_tracked_vehicle_bbs[vehicle_plate][self.camera_id_key+str(camera_id)]:
             # Apply midpoint formula to get center of bb
-            center_pt = [math.ceil((bb[0][0]+bb[1][0])/2), math.ceil((bb[1][0]+bb[1][1])/2)]
-            # print("bb: ", bb)
-            # print("center: ", center_pt)
+            center_pt = [math.ceil((bb[0][0]+bb[1][0])/2), math.ceil((bb[0][1]+bb[1][1])/2)]
+            print("bb: ", bb)
+            print("center: ", center_pt)
             bbs_center_pts.append(center_pt)
 
         return bbs_center_pts
 
-    def buildVehicleBbHistory(self, bbs, vehicle_plate):
+    def buildVehicleBbHistory(self, bbs, camera_id, vehicle_plate):
         bbs = bbs[0]
         # self.latest_tracked_vehicle_frames[camera_id-self.camera_offset][self.count_latest_tracked_vehicle_frames]=bbs
 
         # Add new bb of vehicle
-        self.latest_tracked_vehicle_bbs[vehicle_plate].append(bbs)
+        # self.latest_tracked_vehicle_bbs[vehicle_plate].append(bbs)
+        self.latest_tracked_vehicle_bbs[vehicle_plate][self.camera_id_key+str(camera_id)].append(bbs)
 
         # Pop first bb (found at index 1) if bbs stored exceed maximum number of bbs allowed
         # if len(self.latest_tracked_vehicle_bbs[vehicle_plate]) == self.maximum_tracked_vehicle_bbs:
